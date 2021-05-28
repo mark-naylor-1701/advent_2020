274bbf0715f2bc2c879a5385c96d588b774a6863
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-05-28 14:08:27 -0400

    Clean up the input fetching function

--- src/advent_2020/core.clj
+++ src/advent_2020/core.clj
@@ -1,7 +1,15 @@
 (ns advent-2020.core
-  (:require [clojure.string :refer [split]]))
+  (:require [clojure.string :refer [split join]]))
+
+(def directory "tasks")
+(def separator "/")
+(def eoln #"\n")
 
 (defn input
   "Open the file name, in the tasks directory, read it in, and break into separate lines."
   [file-name]
-  (->> file-name (str "tasks/") (slurp) (#(split % #"\n"))))
+  (->> file-name
+       (list directory)
+       ((partial join separator))
+       (slurp)
+       (#(split % eoln))))
