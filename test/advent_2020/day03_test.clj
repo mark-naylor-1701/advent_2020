0650b54f68b0caea4dcc68b927b62980940f59d1
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-05-28 16:18:45 -0400

    Add Day 2 and Day 2 test modules

---
+++ test/advent_2020/day03_test.clj
@@ -0,0 +1,9 @@
+(ns advent-2020.day03-test
+  (:require [clojure.test :refer :all]
+            [advent-2020.day03 :refer :all]))
+
+;; (deftest valid-count-1-test []
+;;   "Test for part 1 valid count"
+;;   (testing
+;;       "Should be 2 valid passwords."
+;;     (is (= 2 (valid-count in-range? test-data)))))
