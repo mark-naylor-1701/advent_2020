70d4a470695a077a4e09778c73451666a5a9a6b4
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-06-02 13:18:50 -0400

    Add Day 5 and Day 5 test modules

---
+++ test/advent_2020/day06_test.clj
@@ -0,0 +1,8 @@
+(ns advent-2020.day06-test
+  (:require [clojure.test :refer :all]
+            [advent-2020.day06 :refer :all]))
+
+;; (deftest file-name-test
+;;   (testing
+;;       "join now working to build file spec."
+;;     (is (= (join "/" ["tasks" "day01.txt"]) "tasks/day01.txt"))))
