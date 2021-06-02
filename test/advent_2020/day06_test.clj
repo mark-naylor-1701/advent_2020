3087d211765c84310f55e59b3e2ec37a6b08d6da
Mark W. Naylor <mark.naylor.1701@gmail.com>
2021-06-02 14:03:02 -0400

    Create core branch for changing just that module
    Remove the Day 6 modules from core branch, should be forked /before/
    creating day06 branch

--- test/advent_2020/day06_test.clj
+++
@@ -1,8 +0,0 @@
-(ns advent-2020.day06-test
-  (:require [clojure.test :refer :all]
-            [advent-2020.day06 :refer :all]))
-
-;; (deftest file-name-test
-;;   (testing
-;;       "join now working to build file spec."
-;;     (is (= (join "/" ["tasks" "day01.txt"]) "tasks/day01.txt"))))
